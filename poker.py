aada0e552ca0bce514033367195357d0f053f30c
Philippe Lê <le.philippe.duc.tai@gmail.com>
2020-07-28 00:50:14 +0200

    Identification of hands working, but needs more testing. Picking all best 5 cards in sandbox.

--- poker.py
+++ poker.py
@@ -1,5 +1,6 @@
 import random
 import collections 
+import itertools as it
 
 class Deck:
     def __init__(self):
@@ -38,69 +39,103 @@ class Card:
 # A hand is constituted with 7 cards but only the best 5 are taken into account
 # Implements all the logic in poker combinations
 class PokerHandCalculator:
-    def __init__(self):
-        straight_flush_royale_combs = [set(((14,c),(13, c), (12, c), (11, c))) for c in 'schd']
-        straight_flush_combs = [set((x, c) for x in range(i, i+5)) for i in range(2, 14) for c in 'schd']
- 
-    def is_straight_flush_royale(self, seven_card):
-        for comb in self.is_straight_flush_royale_combs:
-            if comb.issubset(seven_card):
+    straight_flush_royale_combs = [set(((14,c),(13, c), (12, c), (11, c), (10, c))) for c in 'schd']
+    straight_flush_combs = [set((x, c) for x in range(i, i+5)) for i in range(2, 11) for c in 'schd'] + [set((x, c) for x in (14, 2, 3, 4, 5)) for c in 'schd']
+    straight_combs = [{14,2,3,4,5}] + [set(x for x in range(i, i+5)) for i in range(2, 11)] 
+
+    def __init__(self, seven_cards):
+        self.seven_cards = seven_cards
+        self.count_values = None
+        self.count_suits = None
+
+    def is_straight_flush_royale(self):
+        for comb in self.straight_flush_royale_combs:
+            if comb.issubset(self.seven_cards):
                 return True
         return False
 
-    def is_straight_flush(self, seven_cards):
-        for comb in self.is_straight_flush_combs:
-            if comb.issubset(seven_card):
+    def is_straight_flush(self):
+        for comb in self.straight_flush_combs:
+            if comb.issubset(self.seven_cards):
                 return True
         return False
 
-    @staticmethod
-    def is_flush(seven_cards):
-        counts_suits = sorted(list(collections.Counter([card.symbol for card in seven_cards]).values()), reverse = True)
-        return counts_suits[0] >= 5 #if more or 5 cards of the same symbol then it is a flush
+    def is_square(self):
+        print(collections.Counter([value for value, _ in self.seven_cards]))
+        self.count_values = sorted(list(collections.Counter([value for value, _ in self.seven_cards]).values()), reverse = True)
+        return max(self.count_values) == 4
 
-    @staticmethod
-    def is_straight(seven_cards):
-        cards_values = list(set(card.value for card in seven_cards))
-        if len(cards_values) < 5 :
-            return False
+    def is_full_house(self):
+        return (self.count_values == [3, 3, 1] or self.count_values == [3, 2, 1, 1] or self.count_values == [3, 2, 2])
+
+    def is_flush(self):
+        self.counts_suits = list(collections.Counter([suit for _, suit in self.seven_cards]).values())
+        return max(self.counts_suits) >= 5 #if more or 5 cards of the same symbol then it is a flush
+
+    def is_straight(self):
+        set_values = set(x for x, _ in self.seven_cards)
+        for comb in self.straight_combs:
+            if comb.issubset(set_values) :
+                return True
+        return False
+
+    def is_three_of_kind(self):
+        return self.count_values == [3, 1, 1, 1, 1]
+
+    def is_two_pairs(self):
+        return self.count_values == [2, 2, 1, 1, 1] or self.count_values == [2, 2, 2, 1]
+
+    def is_pair(self):
+        return self.count_values == [2, 1, 1, 1, 1, 1]
+
+    def evaluate_hand(self):
+        if self.is_straight_flush_royale():
+            rank = 900
+            return ("Straight Flush Royale", rank)
+        elif self.is_straight_flush():
+            rank = 800
+            return ("Straight Flush", rank)
+        elif self.is_square():
+            rank = 700
+            return ("Square", rank)
+        elif self.is_full_house():
+            rank = 600
+            return ("Full House", rank)
+        elif self.is_flush():
+            rank = 500
+            return ("Flush", rank)
+        elif self.is_straight():
+            rank = 400
+            return ("Straight", rank)
+        elif self.is_three_of_kind():
+            rank = 300
+            return ("Three of Kind", rank)
+        elif self.is_two_pairs():
+            rank = 200
+            return ("Two Pairs", rank)
+        elif self.is_pair():
+            rank = 100
+            return ("Pair", rank)
         else :
-            if 14 in cards_values :
-                cards_values.append(1)
-            cards_values.sort() 
-            print(cards_values)
-            counter = 0
-            last = cards_values[0]
-            for v in cards_values[1:] :
-                if v == last + 1 :
-                    counter += 1
-                    print(counter)
-                    if counter == 5 :
-                        return True
+            rank = 0
+            return ("High card", rank)
 
-                elif v == last :
-                    counter = counter
-                else :
-                    counter = 0
-                    
-        return False
 
-    def is_royal_flush(self, seven_cards):
-        pass
 
 # A poker Player has two cards only
 # A hand is a tuple of Cards
 class PokerPlayer:
     def __init__(self, *hand):
         self.hand = hand
-        self.bank = 1000
+        self.str_game = None
 
     def __repr__(self):
-        return str(self.hand)
+        return str(self.str_game)
 
     def combination(self, board):
-        return self.hand + tuple(board)
-
+        strboard = '\033[32m' + str(board)
+        self.str_game = str(self.hand) + strboard + '\033[m'
+        return  [(card.value, card.symbol) for card in it.chain(self.hand, board)]
 
 
 class PokerGame:
@@ -112,7 +147,7 @@ class PokerGame:
         self.board = [self.deck.draw() for _ in range(5)]
 
     def __repr__(self):
-        return "\n".join([str(self.players), str(self.board), str(self.deck)])
+        return "\n".join([str(self.players), str(self.board) , str(self.deck)])
 
     def reinit(self, n_players):
         self.__init__(n_players)
@@ -120,24 +155,23 @@ class PokerGame:
     def hands_combinations(self):
         return [player.combination(self.board) for player in self.players]
             
-
+    def hands_print(self):
+        print([str(player) for player in self.players])
 
 if __name__ == "__main__":
     n_players = 10
     pokergame = PokerGame(n_players)
-    print(pokergame.deck)
     hands = pokergame.hands_combinations()
-    print(hands)
-    counts_values = [sorted(list(collections.Counter([card.value for card in hand]).values()), reverse = True) for hand in hands] 
-    counts_suits = [sorted(list(collections.Counter([card.symbol for card in hand]).values()), reverse = True) for hand in hands] 
+    eval = [PokerHandCalculator(hand).evaluate_hand() for hand in hands]
+    hands_str = "\n".join(map(str, zip(eval, pokergame.players)))
+    print(hands_str)
+
+    # counts_values = [sorted(list(collections.Counter([card.value for card in hand]).values()), reverse = True) for hand in hands] 
+    # counts_suits = [sorted(list(collections.Counter([card.symbol for card in hand]).values()), reverse = True) for hand in hands] 
     
-    
+    # hand = (Qc, 2d)[9s, Kc, 9c, Ac, Jc])
 
-
-    # print(counts_values)
-    # print(counts_suits)
-    # print(counts)
-
-    # peval = PokerHandCalculator()
-    # x = list(map(peval.is_straight, hands))
-    # print(x)
+    # hand = [Card(14,'c'), Card(13, 'c'), Card(2, 'c'), Card(3, 'c'), Card(7,'d'), Card(10,'c'), Card(7,'s')]
+    # hand = [Card(14,'c'), Card(13, 'd'), Card(2, 'd'), Card(3, 'c'), Card(7,'d'), Card(10,'d'), Card(7,'d')]
+    # test = hand_evaluator.is_flush(hand)
+    # print(test)
--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,12 @@
-from collections import namedtuple
+from collections import namedtuple, Counter
+from operator import itemgetter
+import itertools as it
+from functools import reduce
+d = Counter({9: 2, 4: 1, 10: 1, 2: 1, 11: 1, 8: 1})
+d2 = Counter({11: 2, 9: 2, 6: 1, 2: 1, 8: 1})
 
-Card = namedtuple("Card", ['value', 'suit'])
-
-Deck = [Card(x, s) for x in range(2, 15) for s in ('s', 'c', 'h', 'd')]
-
-
+ls = sorted(sorted([(key, value) for key, value in d.items()], reverse = True, key = itemgetter(0)), reverse = True, key = itemgetter(1))
+iterator = it.accumulate(ls, func = lambda acc, x : (x[0], acc[1] + x[1]))
+subit = it.takewhile(lambda x : x[1] <= 5, iterator)
+print(list(subit))
+print(d, d2, sep = '\n')
